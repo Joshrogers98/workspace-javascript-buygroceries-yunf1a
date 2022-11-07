8d290e5a43fc80fb4e925fb333132ffaf83e9942
Joshrogers98 <111779787+Joshrogers98@users.noreply.github.com>
2022-11-07 13:10:11 -0500

    1 file modified

--- js/add-element.js
+++ js/add-element.js
@@ -9,17 +9,25 @@ $('.hot').each(function () {
 });
 
 // traverse the elements
-$('#one').next().next().text('milk');
+$('#two').next().text('milk');
 // add a new element by clicking the plus sign
 $('#todo').append('<li>candy</li>');
 
+console.log('right here')
 // before and after are for siblings
 // append and prepend are for parent
-
+$('#add').click(addElement);
 function addElement() {
   // add a new element
+    //$('#todo').append('<li>cake</li>');
   // add a input text box
+    $('#todo').append('<li><<input type="text"></li>');
   // whenever the user are done add the element
+    $('input').blur(function(){
+      $(this).parent().addClass('cool');
+      var userInput = $(this).val();
+      $(this).parent().text(userInput);
+    });
 }
 
 // bind click with the event handler
@@ -30,6 +38,11 @@ function changeStyle() {
   if ($(this).hasClass('cool')) {
     $(this).removeClass('cool');
     $(this).addClass('complete');
+  }else if($(this).hasClass('complete')){
+    $(this).removeClass('complete');
+    $(this).addClass('cool');
+  }else{
+    $(this).addClass('cool');
   }
 }
 
@@ -38,4 +51,5 @@ document.getElementById('remove').addEventListener('click', removeElement);
 
 function removeElement() {
   // remove the marked elements  -- element with style complete
+  $('li.complete').remove();
 }
